ef60af831bfd27727fca46d1dbd0cdfeaef2279e
Mihai Ocnaru <mihai.ocnaru@mready.net>
2023-08-07 11:09:43 +0300

    Initial Commit

---
+++ DRone/DRoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  DRoneApp.swift
+//  DRone
+//
+//  Created by Mihai Ocnaru on 07.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct DRoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
